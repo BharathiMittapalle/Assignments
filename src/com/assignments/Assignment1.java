96e67d67c524b36e66b83764e43c96e4eb5d9edd
Bharathi <mittapallebharathi19@gmail.com>
2025-01-28 09:57:54 +0000

    Assignment

--- src/com/assignments/Assignment1.java
+++ src/com/assignments/Assignment1.java
@@ -3,7 +3,31 @@ package com.assignments;
 public class Assignment1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		float Temperature = 25.5f;
+		boolean order = true;
+		long phone = 1234567890;
+		float amount = 1000.50f;
+		String mailid = "john.deo@gmail.com";
+		double latitude = 37.7749;
+		double longitude = -122.4194;
+		boolean status = true;
+		String occupation = "Software Engineer";
+		String colour = "Blue";
+		short currentyear = 2023;
+		String mediaplatform = "1,00,0000";      
+		float rateofmovie = 2.5f;
+		char bloodtype = 'A';
+		String titleofbook = "To Kill a Mockingbird";
+		short empno = 500;
+		String timeofevent = "2:30 PM";        
+		String countryname = "United States";
+		String eyecolour = "Brown";
+		String birthplace = "New york City";
+		float disbetcities = 200.5f;
+		
+	
+		
+		
 
 	}
 
---
+++ src/com/assignments/Assignment2.java
@@ -0,0 +1,14 @@
+package com.assignments;
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		
+		String [] subjects = {"Mathematics I, Physics, Chemistry, Computer Programming, Engineering Drawing, Basic Electrical Eng"};
+		String [] Marks = {"78, 85, 91, 74, 88, 79"};
+		String [] sub2 = {"Mathematics, II Mechanics,Environmental Sci, Basic Electronics, Engineering Physics, Engineering Graphics"};
+		String [] marks = {"82, 77, 93, 69, 84, 90"};
+		
+		
+	}
+}
