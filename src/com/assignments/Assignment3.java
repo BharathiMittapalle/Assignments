a8c768bf3e0b9c93182e4d5ecbfa10c4765d882e
Bharathi <mittapallebharathi19@gmail.com>
2025-02-05 10:55:02 +0000

    Assignment3

--- src/com/assignments/Assignment3.java
+++ src/com/assignments/Assignment3.java
@@ -1,8 +1,6 @@
 package com.assignments;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
+
 
 public class Assignment3 {
 
@@ -10,23 +8,18 @@ public class Assignment3 {
 	         String val1 = "Bharathi";
 	  static String val2 = "Bharathi M";
 	
-	//public static void main(String[] args) {
+	public static void main(String[] args) {
 		
 		//System.out.println(val1);
-		//System.out.println(val2);
+		System.out.println(val2);
+		
+		Assignment3 obj = new Assignment3();
+		System.out.println(obj.val1);
 		
 		
-		//public class reverse array { 
-			public static void main(String[] args) { 
-			int [] Array ={7,8,9,3,4,6,11,67,98}; 
-			int k = Array.length-1; 
-			for(k = Array.length-1;k>=0;k--){ 
-			System.out.print( Array[k] + " "); 
-			} 
-			}
 		
 			
 			
 	}
 
-//}
+}
