03bfb7150a3877e9bbd09084284bf86237146640
Bharathi <mittapallebharathi19@gmail.com>
2025-02-05 10:33:02 +0000

    Assignment3

--- src/com/assignments/Assignment2.java
+++ src/com/assignments/Assignment2.java
@@ -1,6 +1,9 @@
 package com.assignments;
 
 public class Assignment2 {
+	
+	String[] val = { "Bharathi" , "xyz","ABC"};
+	
 
 	public static void main(String[] args) {
 		
@@ -8,6 +11,11 @@ public class Assignment2 {
 		String [] Marks = {"78, 85, 91, 74, 88, 79"};
 		String [] sub2 = {"Mathematics, II Mechanics,Environmental Sci, Basic Electronics, Engineering Physics, Engineering Graphics"};
 		String [] marks = {"82, 77, 93, 69, 84, 90"};
+		
+		Assignment2 obj = new Assignment2();
+		
+		
+		System.out.println(obj.val[0]);
 		
 		
 	}
---
+++ src/com/assignments/Assignment3.java
@@ -0,0 +1,32 @@
+package com.assignments;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+public class Assignment3 {
+
+	
+	         String val1 = "Bharathi";
+	  static String val2 = "Bharathi M";
+	
+	//public static void main(String[] args) {
+		
+		//System.out.println(val1);
+		//System.out.println(val2);
+		
+		
+		//public class reverse array { 
+			public static void main(String[] args) { 
+			int [] Array ={7,8,9,3,4,6,11,67,98}; 
+			int k = Array.length-1; 
+			for(k = Array.length-1;k>=0;k--){ 
+			System.out.print( Array[k] + " "); 
+			} 
+			}
+		
+			
+			
+	}
+
+//}
